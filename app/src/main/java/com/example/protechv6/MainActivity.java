aba5a0a5ef8e41562c1613199d907233baaf7185
Tala <kvillaroman07@yahoo.com.ph>
2023-02-07 22:39:04 +0800

    test

--- app/src/main/java/com/example/protechv6/MainActivity.java
+++ app/src/main/java/com/example/protechv6/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
 
         loginText = (EditText) findViewById(R.id.login);
         passwordText = (EditText) findViewById(R.id.password);
