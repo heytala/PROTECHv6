f1de07e8d48b3c475b95ac985746e6c1f3afadc0
Tala <kvillaroman07@yahoo.com.ph>
2023-02-08 22:40:34 +0800

    Working list view that retrieves items from db. Needs code cleaning

--- app/src/main/java/com/example/protechv6/SensorActivity.java
+++ app/src/main/java/com/example/protechv6/SensorActivity.java
@@ -19,7 +19,9 @@ import com.google.firebase.database.ValueEventListener;
 import com.example.protechv6.databinding.ActivityMainBinding;
 import com.example.protechv6.databinding.ActivitySensorBinding;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class SensorActivity extends AppCompatActivity {
 
@@ -31,18 +33,44 @@ public class SensorActivity extends AppCompatActivity {
     DatabaseReference motionRef;
     DatabaseReference doorRef;
     DatabaseReference windowRef;
-    DatabaseReference triggerEmergencyRef;
+    DatabaseReference flameRef;
     DatabaseReference smokeRef;
+    DatabaseReference triggerEmergencyRef;
+
+    DatabaseReference motionLtRef;
+    DatabaseReference doorLtRef;
+    DatabaseReference windowLtRef;
+    DatabaseReference flameLtRef;
+    DatabaseReference smokeLtRef;
+
 
 
     private TextView retrieveTV;
 
+    private static int motionVal = 0;
+    private static int doorVal = 0;
+    private static int windowVal = 0;
+    private static int flameVal = 0;
+    private static int smokeVal = 0;
+    private static int triggerEmergencyVal = 0;
+
+
     private static String value;
-    private static int motion;
-    private static int door;
-    private static int window;
-    private static int triggerEmergency;
-    private static int smoke;
+    private static String motion = " ";
+    private static String door = " ";
+    private static String window = " ";
+    private static String flame = " ";
+    private static String smoke = " ";
+    private static String triggerEmergency = " ";
+
+    private static String ltMotion  = " ";
+    private static String ltDoor    = " ";
+    private static String ltWindow  = " ";
+    private static String ltFlame   = " ";
+    private static String ltSmoke   = " ";
+
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy ' ' HH:mm:ss z");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,17 +80,26 @@ public class SensorActivity extends AppCompatActivity {
 
         //setContentView(R.layout.activity_sensor);
 
-        firebaseDatabase        = FirebaseDatabase.getInstance();
 
-        databaseReference      = firebaseDatabase.getReference("Sensor").child("button_state");
-        motionRef              = firebaseDatabase.getReference("Sensor").child("motion");
-        doorRef                = firebaseDatabase.getReference("Sensor").child("door");
-        windowRef              = firebaseDatabase.getReference("Sensor").child("window");
-        triggerEmergencyRef    = firebaseDatabase.getReference("Sensor").child("triggerEmergency");
-        smokeRef               = firebaseDatabase.getReference("Sensor").child("smoke");
+        firebaseDatabase         = FirebaseDatabase.getInstance();
+
+        databaseReference        = firebaseDatabase.getReference("Sensor").child("button_state");
+        motionRef                = firebaseDatabase.getReference("motion").child("val");
+        doorRef                  = firebaseDatabase.getReference("door").child("val");
+        windowRef                = firebaseDatabase.getReference("window").child("val");
+        flameRef                 = firebaseDatabase.getReference("flame").child("val");
+        smokeRef                 = firebaseDatabase.getReference("smoke").child("val");
+        triggerEmergencyRef      = firebaseDatabase.getReference("triggerEmergency").child("val");
+
+
+        motionLtRef              = firebaseDatabase.getReference("motion").child("lt");
+        doorLtRef                = firebaseDatabase.getReference("door").child("lt");
+        windowLtRef              = firebaseDatabase.getReference("window").child("lt");
+        flameLtRef              = firebaseDatabase.getReference("flame").child("lt");
+        smokeLtRef               = firebaseDatabase.getReference("smoke").child("lt");
 
         retrieveTV = findViewById(R.id.idTVRetrieveData);
-        getdata();
+        getData();
 
         Button button = (Button) findViewById(R.id.buttonLogout);
         button.setOnClickListener(new View.OnClickListener() {
@@ -92,43 +129,241 @@ public class SensorActivity extends AppCompatActivity {
     }
 
 
-    private void getdata() {
+    private void getData() {
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 value = snapshot.getValue(String.class);
                 retrieveTV.setText(value);
-
-                int[] imageId = {R.drawable.a, R.drawable.a, R.drawable.a, R.drawable.a, R.drawable.a, R.drawable.a};
-                String[] sensor = {"Motion","Door","Window","Flame","Smoke","Video Surveillance"};
-                String[] status = {"Status: " + value, "Status: " + value, "Status: " + value, "Status: " + value, "Status: " + value, "Status: " + value};
-                String[] lastTriggered = {"8:45 pm","9:00 am","7:34 pm","6:32 am","5:76 am", "5:00 am"};
-
-                ArrayList<Sensors> sensorArrayList = new ArrayList<>();
-
-                for(int i = 0;i< imageId.length;i++){
-                    Sensors sensorsArr = new Sensors(sensor[i],status[i],lastTriggered[i],imageId[i]);
-                    sensorArrayList.add(sensorsArr);
-                }
-
-                ListAdapter listAdapter = new ListAdapter(SensorActivity.this,sensorArrayList);
-                binding.listview.setAdapter(listAdapter);
-                binding.listview.setClickable(true);
-
-
+                dataArr();
             }
-
-
-
-            @Override
+         @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(SensorActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
             }
-            
-
-
         });
+
+//GET DATE AND TIME LAST TRIGGERED: MOTION
+        motionLtRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ltMotion = snapshot.getValue(String.class);
+                dataArr();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("Fail to get data: MOTION LT");
+            }
+        });
+
+//GET DATE AND TIME LAST TRIGGERED: DOOR
+        doorLtRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ltDoor = snapshot.getValue(String.class);
+                dataArr();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("Fail to get data: DOOR LT");
+            }
+        });
+
+//GET DATE AND TIME LAST TRIGGERED: WINDOW
+        windowLtRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ltWindow = snapshot.getValue(String.class);
+                dataArr();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("Fail to get data: WINDOW LT");
+            }
+        });
+
+//GET DATE AND TIME LAST TRIGGERED: WINDOW
+        windowLtRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ltWindow = snapshot.getValue(String.class);
+                dataArr();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("Fail to get data: WINDOW LT");
+            }
+        });
+
+//GET DATE AND TIME LAST TRIGGERED: FLAME
+        flameLtRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ltFlame = snapshot.getValue(String.class);
+                dataArr();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("Fail to get data: FLAME LT");
+            }
+        });
+
+//GET DATE AND TIME LAST TRIGGERED: SMOKE
+        smokeLtRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ltSmoke = snapshot.getValue(String.class);
+                dataArr();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("Fail to get data: SMOKE LT");
+            }
+        });
+
+//GET MOTION VALUE
+        motionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                motionVal = snapshot.getValue(int.class);
+
+                if (motionVal == 1) {
+                    motion = "Motion Detected!";
+                    ltMotion = sdf.format(new Date());
+                    motionLtRef.setValue(ltMotion);
+                } else if (motionVal == 0){
+                    motion = "No Motion";
+                }
+                dataArr();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(SensorActivity.this, "Fail to get data: MOTION", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//GET DOOR VALUE
+        doorRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                doorVal = snapshot.getValue(int.class);
+
+                if (doorVal == 1) {
+                    door = "Open";
+                    ltDoor = sdf.format(new Date());
+                    doorLtRef.setValue(ltDoor);
+                } else if (doorVal == 0){
+                    door = "Closed";
+                }
+                dataArr();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(SensorActivity.this, "Fail to get data: DOOR", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//GET WINDOW VALUE
+        windowRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                windowVal = snapshot.getValue(int.class);
+
+                if (windowVal == 1) {
+                    window = "Open";
+                    ltWindow = sdf.format(new Date());
+                    windowLtRef.setValue(ltWindow);
+                } else if (windowVal == 0){
+                    window = "Closed";
+                }
+                dataArr();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(SensorActivity.this, "Fail to get data: WINDOW", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//GET FLAME VALUE
+        flameRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                flameVal = snapshot.getValue(int.class);
+
+                if (flameVal == 0) {
+                    flame = "Flame Detected!";
+                    ltFlame = sdf.format(new Date());
+                    flameLtRef.setValue(ltFlame);
+                } else if (flameVal == 1){
+                    flame = "Normal";
+                }
+                dataArr();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(SensorActivity.this, "Fail to get data: FLAME", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//GET SMOKE VALUE
+        smokeRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                smokeVal = snapshot.getValue(int.class);
+
+                if (smokeVal == 1) {
+                    smoke = "Lvl 1 (Normal)";
+                } else if (smokeVal == 2){
+                    smoke = "Lvl 2";
+                } else if (smokeVal == 3){
+                    smoke = "Lvl 3";
+                    ltSmoke = sdf.format(new Date());
+                    smokeLtRef.setValue(ltSmoke);
+                }
+                dataArr();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(SensorActivity.this, "Fail to get data: SMOKE", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//GET TRIGGER EMERGENCY VALUE
+        triggerEmergencyRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                triggerEmergencyVal = snapshot.getValue(int.class);
+
+                //Insert business logic
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(SensorActivity.this, "Fail to get data: TRIGGER EMERGENCY", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private ArrayList dataArr() {
+        int[] imageId = {R.drawable.a, R.drawable.a, R.drawable.a, R.drawable.a, R.drawable.a, R.drawable.a};
+        String[] sensor = {"Motion","Door","Window","Flame","Smoke","Video Surveillance"};
+        String[] status = {"Status: " + motion, "Status: " + door, "Status: " + window, "Status: " + flame, "Status: " + smoke, "Status: Running"};
+        String[] lastTriggered = {ltMotion,ltDoor,ltWindow,ltFlame,ltSmoke, " "};
+
+        ArrayList<Sensors> sensorArrayList = new ArrayList<>();
+
+        for(int i = 0;i< imageId.length;i++){
+            Sensors sensorsArr = new Sensors(sensor[i],status[i],lastTriggered[i],imageId[i]);
+            sensorArrayList.add(sensorsArr);
+        }
+
+        ListAdapter listAdapter = new ListAdapter(SensorActivity.this,sensorArrayList);
+        binding.listview.setAdapter(listAdapter);
+        binding.listview.setClickable(true);
+
+        return sensorArrayList;
+
     }
 
 
