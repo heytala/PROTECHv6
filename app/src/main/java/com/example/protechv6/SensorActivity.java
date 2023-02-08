2555cfae1051ced11ef2d9914c5d4b9bbb4efc71
Tala <kvillaroman07@yahoo.com.ph>
2023-02-08 19:11:48 +0800

    Clean up code before coding retrieve value for each sensor

--- app/src/main/java/com/example/protechv6/SensorActivity.java
+++ app/src/main/java/com/example/protechv6/SensorActivity.java
@@ -3,6 +3,7 @@ package com.example.protechv6;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,16 +28,21 @@ public class SensorActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
 
     DatabaseReference databaseReference;
-    DatabaseReference motion;
-    DatabaseReference door;
-    DatabaseReference window;
-    DatabaseReference triggerEmergency;
-    DatabaseReference smoke;
+    DatabaseReference motionRef;
+    DatabaseReference doorRef;
+    DatabaseReference windowRef;
+    DatabaseReference triggerEmergencyRef;
+    DatabaseReference smokeRef;
 
 
     private TextView retrieveTV;
 
     private static String value;
+    private static int motion;
+    private static int door;
+    private static int window;
+    private static int triggerEmergency;
+    private static int smoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +52,14 @@ public class SensorActivity extends AppCompatActivity {
 
         //setContentView(R.layout.activity_sensor);
 
-        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase        = FirebaseDatabase.getInstance();
 
-        databaseReference   = firebaseDatabase.getReference("Sensor").child("button_state");
-        motion              = firebaseDatabase.getReference("Sensor").child("motion");
-        door                = firebaseDatabase.getReference("Sensor").child("door");
-        window              = firebaseDatabase.getReference("Sensor").child("window");
-        triggerEmergency    = firebaseDatabase.getReference("Sensor").child("triggerEmergency");
-        smoke               = firebaseDatabase.getReference("Sensor").child("smoke");
-
+        databaseReference      = firebaseDatabase.getReference("Sensor").child("button_state");
+        motionRef              = firebaseDatabase.getReference("Sensor").child("motion");
+        doorRef                = firebaseDatabase.getReference("Sensor").child("door");
+        windowRef              = firebaseDatabase.getReference("Sensor").child("window");
+        triggerEmergencyRef    = firebaseDatabase.getReference("Sensor").child("triggerEmergency");
+        smokeRef               = firebaseDatabase.getReference("Sensor").child("smoke");
 
         retrieveTV = findViewById(R.id.idTVRetrieveData);
         getdata();
@@ -68,21 +73,21 @@ public class SensorActivity extends AppCompatActivity {
             }
         });
 
+/*        binding.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-//        int[] imageId = {R.drawable.a};
-//        String[] sensor = {"Motion","Door","Window","Flame","Smoke","Video Surveillance"};
-//        String[] status = {"Status: " + value, "Status: " + value};
-//        String[] lastTriggered = {"8:45 pm","9:00 am","7:34 pm","6:32 am","5:76 am", "5:00 am"};
-//
-//        ArrayList<Sensors> sensorArrayList = new ArrayList<>();
-//
-//        for(int i = 0;i< imageId.length;i++){
-//            Sensors sensorsArr = new Sensors(sensor[i],status[i],lastTriggered[i],imageId[i]);
-//            sensorArrayList.add(sensorsArr);
-//        }
-//
-//        ListAdapter listAdapter = new ListAdapter(SensorActivity.this,sensorArrayList);
-//        binding.listview.setAdapter(listAdapter);
+                Intent i = new Intent(SensorActivity.this,VideoActivity.class);
+                i.putExtra("name",name[position]);
+                i.putExtra("phone",phoneNo[position]);
+                i.putExtra("country",country[position]);
+                i.putExtra("imageid",imageId[position]);
+                startActivity(i);
+
+            }
+        });*/
+
+
 
     }
 
@@ -109,14 +114,24 @@ public class SensorActivity extends AppCompatActivity {
 
                 ListAdapter listAdapter = new ListAdapter(SensorActivity.this,sensorArrayList);
                 binding.listview.setAdapter(listAdapter);
+                binding.listview.setClickable(true);
+
+
             }
+
+
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                // calling on cancelled method when we receive
-                // any error or we are not able to get the data.
                 Toast.makeText(SensorActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
             }
+            
+
+
         });
     }
+
+
+
+
 }
